49d3c14d0ad3f7613525b57264b24e01a09a0665
Dipesh Ghimire <ghimiredipesh33@gmail.com>
2026-08-05 08:53:06 +0545

    third commit

--- Assignment_1.py
+++ Assignment_1.py
@@ -11,3 +11,11 @@ print(res)  # Printing the dictionary of words with length greater than 4 and th
 
 letters={i for i in sent if i.isalpha()} 
 print(letters)  # Printing the set of unique letters in the sentence
+
+
+# Data Cleaning and Processing
+
+raw_data=['150', 'invalid', '400','80' '300', 'error', '250','999','1500']
+
+cleaned_data = [int(x) for x in raw_data if x.isdigit() if int(x) >= 150 and int(x)<=999]  # Cleaning the data by filtering out non-numeric values and values less than or equal to 100
+print(cleaned_data)  # Printing the list of cleaned numeric values
