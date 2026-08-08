db7cf1658c207753271d5e87d0e6a99eb1b200d6
Dipesh Ghimire <ghimiredipesh33@gmail.com>
2026-08-08 10:31:59 +0545

    Python: Checking if input no is armstrong or not

--- Assignment/Assignment_2/Task_4.py
+++ Assignment/Assignment_2/Task_4.py
@@ -1,6 +1,7 @@
-# Armstrong number in python
+# Armstrong number in python using String as input
 
-num = input ( " Enter a number to check if Armstrong or not : ")
+num = input ( " Enter a positive number to check if Armstrong or not : ")
+# num=num.strip("-")                    # for forcing the string to not have "-" sign
 s=0
 for i in num:
    s=int(i)**len(num)+s
@@ -10,8 +11,18 @@ if str(s) == num:
 else:
    print(f"{num} is not an armstrong no")
 
+# Armstrong number in python using integer as input
 
+num = int ( input ( "enter a positive no :"))
+s=0
+for i in str(num):
+   s= int(i) ** len(str(num)) + s
 
+if s == num:
+   print(f"{num} is an armstrong")
+else:
+   print(f"{num} is not an armstrong no")
+   
 
 
 
