da03d5161dc163d5ce8ff15fc38a8c4e0ca8701d
Dipesh Ghimire <ghimiredipesh33@gmail.com>
2026-08-09 10:48:57 +0545

    Python: Removing null values in given set of data

---
+++ Day_wise_learning/Day_4/null.py
@@ -0,0 +1,11 @@
+raw_data = [ ["Ram","10",""],["Shyam","12","Kathmandu"],[" "," 15","Bhaktapur"]]
+
+clean_data=[]
+for x in raw_data:
+    name=x[0].strip()
+    age=x[1].strip()
+    address=x[2].strip()
+    if name and age and address:
+        clean_data.append(x)
+
+print(clean_data)
