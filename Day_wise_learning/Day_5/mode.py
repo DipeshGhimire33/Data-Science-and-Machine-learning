bbb2bea53ef146b7abbc3f5df76d660c0ab96b90
Dipesh Ghimire <ghimiredipesh33@gmail.com>
2026-08-10 14:37:20 +0545

    Python: calculating mode from counter, statistics and without using libraries

--- Day_wise_learning/Day_5/mode.py
+++ Day_wise_learning/Day_5/mode.py
@@ -17,3 +17,23 @@ mult_data = [1,15,32,45,8,66,74,34,66,95,15]
 modes = statistics.multimode(mult_data)
 print(modes)  
 
+# without any libraries
+
+data =[1,1,2,2,3,3,3,4,5,5]
+count_dic ={}
+data.sort()
+
+for i in data:
+    count=0
+    for j in range(len(data)):
+        if i == data[j]:
+            count= count +1
+    count_dic[i]=count
+print(count_dic) 
+
+freq=[]
+for num in count_dic:
+    freq.append(count_dic[num])
+    freq.sort(reverse=True)
+    mode = next(k for k,v in count_dic.items() if v==freq[0])
+print(mode)
