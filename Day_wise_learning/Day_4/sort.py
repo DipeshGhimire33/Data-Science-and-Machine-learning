de3cd96f00bb4a3806626a6b096aa9a16ece3419
Dipesh Ghimire <ghimiredipesh33@gmail.com>
2026-08-10 08:37:05 +0545

    Python: Program performing sorting operation using lambda

--- Day_wise_learning/Day_4/sort.py
+++ Day_wise_learning/Day_4/sort.py
@@ -1,11 +1,13 @@
-raw_data = [["15","Ram","Kathmandu"],["10","Shyam","Biratnagar"],["20","Kiran","Jhapa"],["1","Isha","Bhaktapur"]]
+raw_data = [["15","Ram","Kathmandu"],["10","Shyam","Biratnagar"],
+            ["20","Kiran","Jhapa"],["1","Isha","Bhaktapur"]]
 
+# sorting based on id
 sorted_data=[]
 for person in range(len(raw_data)):
     sorted_data = sorted(raw_data,key = lambda x:x[0])
 print(sorted_data)     
 
-
+# for sorting based on address
 for person in range(len(raw_data)):
     raw_data.sort(key = lambda x:x[2])
 print(raw_data)  
