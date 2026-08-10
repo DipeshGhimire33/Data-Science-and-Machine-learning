3d9e327092c23a322ff8b14bc28dd31c9bb30dd1
Dipesh Ghimire <ghimiredipesh33@gmail.com>
2026-08-10 08:34:10 +0545

    Python: Program performing standard deviation calc using args in function

--- Day_wise_learning/Day_5/std_dev.py
+++ Day_wise_learning/Day_5/std_dev.py
@@ -1,10 +1,18 @@
 # to calculate standard deviation from the given list of numbers
 
-def std_calc(*numbers):
+
+
+def std_calc(*numbers): 
+    """_argument: numbers_
+
+    Returns:
+        _list_: _give list of user input numbers_
+    """                        
     N =len(numbers)
     mean = sum(numbers)/N
     numerator=sum((number -mean)**2 for number in numbers)
     std= (numerator/N)**0.5
     return std
-
+            
 print(round(std_calc(1,5,6,7,6,9,1.5,93,4),2))
+
