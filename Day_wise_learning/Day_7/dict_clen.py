c5d8f75fcdb5b41f559f7978582bc10da067fdf4
Dipesh Ghimire <ghimiredipesh33@gmail.com>
2026-08-12 08:08:04 +0545

    Python : Performing dictionary flattenning operation

--- Day_wise_learning/Day_7/dict_clen.py
+++ Day_wise_learning/Day_7/dict_clen.py
@@ -16,29 +16,24 @@ nested_data ={
 expected_dict = {}
 
 def flatten_list(data, parent_key=""):
-    for k, v in data.items():
-        new_key = f"{parent_key}.{k}" if parent_key else k
-
-        if isinstance(v, dict):
-            flatten_list(v, new_key)
-
-        elif isinstance(v, list):
+    for k,v in data.items():
+        new_key=f"{parent_key}.{k}" if parent_key else k
+        if isinstance(v,dict):
+            flatten_list(v,new_key)
+        elif isinstance(v,list):
             for i, item in enumerate(v):
-                list_key = f"{new_key}.{i}"
-                
-                if isinstance(item, dict):
-                    flatten_list(item, list_key)
+                list_key= f"{new_key}.{i}"
+
+                if isinstance(item,dict):
+                    flatten_list(item,list_key)
                 else:
-                    expected_dict[list_key] = item
-
+                    expected_dict[list_key]=item
+                 
         else:
-            expected_dict[new_key] = v
-
+              expected_dict[new_key]=v
 
 flatten_list(nested_data)
-
 print(expected_dict)
-
 
 # Excepted Dict:
 # {
