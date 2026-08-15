cd8b5f03535a97e1d4511767373ae5d13d110ea8
Dipesh Ghimire <ghimiredipesh33@gmail.com>
2026-08-15 14:28:02 +0545

    Python: Lambda function for checking palindrome

---
+++ Assignment/Assignment_3/Task1.py
@@ -0,0 +1,7 @@
+
+is_palindrome = lambda word: "palindrome" if word == word[::-1] else "not palindrome"
+print(is_palindrome("racecar"))
+
+
+
+# lambda "arguments" : "output" for given condition else "else output"
